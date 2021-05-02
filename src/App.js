33114ef67613a82576d18135c92b5abcdd795fc6
Ian Marshall <imarshall14@gmail.com>
2021-05-02 18:38:26 -0400

    commit tree 9450c45d5377f93ce47ccb275e79ae20cad8b762

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
@@ -13,4 +14,5 @@ export class App extends Component {
   }
 };
 
-export default App;
+
+export default App
